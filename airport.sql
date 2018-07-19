1ea4150a70f610b9b3a09ba91596d9cbbe3850ec
Brian <39497182+bpalowski@users.noreply.github.com>
2018-07-19 12:01:28 -0700

    Add files via upload

--- airport.sql
+++ airport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 17, 2018 at 01:27 AM
+-- Generation Time: Jul 19, 2018 at 09:01 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `airport`
 --
-CREATE DATABASE IF NOT EXISTS `airport` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `airport`;
 
 -- --------------------------------------------------------
 
@@ -46,7 +44,35 @@ INSERT INTO `cities` (`id`, `city`, `state`) VALUES
 (6, 'Bainbridge', 'WA'),
 (7, 'Wallawalla', 'WA'),
 (8, 'g', 'dfsf'),
-(9, 'New Seattle', 'WA');
+(9, 'New Seattle', 'WA'),
+(10, 'new', 'WA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities_flights`
+--
+
+CREATE TABLE `cities_flights` (
+  `id` int(11) NOT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  `flight_number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cities_flights`
+--
+
+INSERT INTO `cities_flights` (`id`, `city_id`, `flight_number`) VALUES
+(1, 1, 100),
+(2, 1, 101),
+(7, 1, 400),
+(8, 2, 200),
+(9, 2, 100),
+(10, 1, 500),
+(11, 2, 600),
+(12, 4, 607),
+(13, 3, 200);
 
 -- --------------------------------------------------------
 
@@ -58,7 +84,7 @@ CREATE TABLE `flights` (
   `id` int(11) NOT NULL,
   `flight_number` int(11) DEFAULT NULL,
   `depart_time` varchar(11) DEFAULT NULL,
-  `depart_id` int(11) DEFAULT NULL,
+  `depart_id` int(11) NOT NULL,
   `arrive_id` int(11) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -68,18 +94,14 @@ CREATE TABLE `flights` (
 --
 
 INSERT INTO `flights` (`id`, `flight_number`, `depart_time`, `depart_id`, `arrive_id`, `status`) VALUES
-(1, 307, '3:00 PM', 1, 2, 'on time'),
-(2, 308, '7:00 PM', 3, 4, 'KIA'),
-(3, 102, '2:00 PM', 2, 1, '1'),
-(4, 102, '2:00 PM', 2, 1, 'xnld'),
-(5, 102, '2:00 PM', 2, 1, 'xnld'),
-(6, 102, '2:00 PM', 2, 1, 'xnld'),
-(7, 1, '2:00 PM', 1, 1, 'sdf'),
-(8, 222, '5:00pm', 22, 22222, '22323dx'),
-(9, 3333, '6:00pm', 0, 0, 'open'),
-(10, 333, '3:00pm', 322, 23, 'xxx'),
-(11, 123, '1:00pm', 1, 2, 'on time'),
-(12, 123, 'now', 9, 1, 'good to go!');
+(23, 100, '1', 1, 1, 'a'),
+(25, 101, '1', 1, 1, 'a'),
+(32, 400, '1', 1, 1, 'a'),
+(33, 200, '1', 1, 1, 'a'),
+(35, 500, '1', 1, 1, 'a'),
+(36, 600, '1', 2, 1, 'good to go!'),
+(37, 607, '4:00pm', 4, 5, 'open'),
+(38, 200, '8:00pm', 3, 3, 'open');
 
 --
 -- Indexes for dumped tables
@@ -89,6 +111,12 @@ INSERT INTO `flights` (`id`, `flight_number`, `depart_time`, `depart_id`, `arriv
 -- Indexes for table `cities`
 --
 ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cities_flights`
+--
+ALTER TABLE `cities_flights`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -105,12 +133,17 @@ ALTER TABLE `flights`
 -- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `cities_flights`
+--
+ALTER TABLE `cities_flights`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `flights`
 --
 ALTER TABLE `flights`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
